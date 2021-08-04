1f7c77e1cfb4c219614f37cfbe3939a97c87a316
Adithya Kamath <adithyakamath96@gmail.com>
2021-08-05 02:45:58 +0530

    Adding bootstrap like library

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-const TrackListScreen = ({navigation}) => {
+const TrackListScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Track List Screen</Text>
+      <Button
+        title="Go to Track Detail"
+        onPress={() => navigation.navigate("TrackDetail")}
+      />
+    </View>
+  );
+};
 
-    return (
+export default TrackListScreen;
 
-        <View>
-            <Text>Track List Screen</Text>
-            <Button title="Go to Track Detail" onPress={()=> navigation.navigate('TrackDetail')}/>
-        </View>
-    )
-}
-
-export default TrackListScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
