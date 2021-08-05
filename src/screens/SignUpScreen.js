c1a8ed6194bb9887858930e63c53fb828ca7d54b
kamath7 <adithyakamath96@gmail.com>
2021-08-05 22:28:31 +0530

    Adding navigation options

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { StyleSheet, View, } from "react-native";
-import {Text,Input,  Button} from 'react-native-elements'
+import { StyleSheet, View } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignUpScreen = ({ navigation }) => {
   return (
     <>
-    <Spacer>
-      <Text h3>Sign Up for Master Tracker</Text>
-    </Spacer>
-      <Input label="Email"/>
-      <Spacer/>
-      <Input label="Password"/>
-      
       <Spacer>
-      <Button title="Sign Up"/>
+        <Text h3>Sign Up for Master Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+
+      <Spacer>
+        <Button title="Sign Up" />
       </Spacer>
     </>
   );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
 };
 
 export default SignUpScreen;
