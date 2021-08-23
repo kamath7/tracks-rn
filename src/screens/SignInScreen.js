dcf21d009cba555c5fde6220b4b2fd7d7dfe95bc
kamath7 <adithyakamath96@gmail.com>
2021-08-23 22:57:30 +0530

    initial sign in screen established

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,14 +1,41 @@
 import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 const SignInScreen = () => {
   return (
-    <View>
-      <Text>Sign In Screen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText={"Sign In"}
+        onSubmit={() => {}}
+        errorMessage={""}
+        submitButtonText={"Sign In"}
+      />
+      <NavLink text={"Sign Up here if no account"} routeName={"SignUp"} />
     </View>
   );
 };
 
+SignInScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
 export default SignInScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
+/*
+SigninScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+*/
