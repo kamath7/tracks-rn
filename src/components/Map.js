bcc34435053bb9e910181d517e0dc60f47a8c23d
kamath7 <adithyakamath96@gmail.com>
2021-08-24 23:29:40 +0530

    map component added and minor changes

---
+++ src/components/Map.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import {Text} from 'react-native-elements'
+const Map = () => {
+    return (
+        <View>
+            <Text>Is a map!</Text>
+        </View>
+    )
+}
+
+export default Map
+
+const styles = StyleSheet.create({})
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-
+import { StyleSheet,  View } from 'react-native'
+import {Text} from 'react-native-elements'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Map from '../components/Map'
 const TrackCreateScreen = () => {
     return (
-        <View>
-            <Text>Track Create Screen</Text>
-        </View>
+        <SafeAreaView forceInset={{top:'always'}}>
+            <Text h3>Track Create Screen</Text>
+            <Map/>
+        </SafeAreaView>
     )
 }
 
