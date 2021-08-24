71601b2ea40a6344e7ff1beea7753676b22aec71
kamath7 <adithyakamath96@gmail.com>
2021-08-24 23:13:29 +0530

    to avoid overlap

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,17 +1,18 @@
 import React, {useContext} from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-elements";
+import {SafeAreaView} from 'react-navigation'
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/authContext";
 const AccountScreen = () => {
     const{ signout} = useContext(AuthContext)
   return (
-    <>
+    <SafeAreaView forceInset={{top: 'always'}}>
       <Text>Account Screen</Text>
       <Spacer>
       <Button title="Sign Out" onPress={signout}/>
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 
