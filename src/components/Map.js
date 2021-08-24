751985df68e9724b705f40df5ea79cf355f5137e
kamath7 <adithyakamath96@gmail.com>
2021-08-24 23:48:19 +0530

    drawing lines over routes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,42 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import {Text} from 'react-native-elements'
-import MapView from 'react-native-maps'
-const Map = () => {
-    return (
-      <MapView style={styles.map} initialRegion={{
-          latitude:12.9141,
-          longitude:74.8560,
-          latitudeDelta:0.01, //basically zoom levels
-          longitudeDelta: 0.01
-      }}/>
-    )
-}
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Text } from "react-native-elements";
+import MapView, { Polyline } from "react-native-maps";
 
-export default Map
+const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 12.9141 + i * 0.001,
+        longitude: 74.856 + i * 0.001,
+      });
+    } else {
+      points.push({
+        latitude: 12.9141 - i * 0.002,
+        longitude: 74.856 - i * 0.002,
+      });
+    }
+  }
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 12.9141,
+        longitude: 74.856,
+        latitudeDelta: 0.01, //basically zoom levels
+        longitudeDelta: 0.01,
+      }}
+    >
+      <Polyline lineDashPattern={[4]} coordinates={points} />
+    </MapView>
+  );
+};
+
+export default Map;
 
 const styles = StyleSheet.create({
-    map:{
-        height:300
-    }
-})
+  map: {
+    height: 300,
+  },
+});
