21edad67e4c601d560c8946e73022f2f7cdcbb49
kamath7 <adithyakamath96@gmail.com>
2021-08-24 23:39:44 +0530

    using mapview

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import {Text} from 'react-native-elements'
+import MapView from 'react-native-maps'
 const Map = () => {
     return (
-        <View>
-            <Text>Is a map!</Text>
-        </View>
+      <MapView style={styles.map} initialRegion={{
+          latitude:12.9141,
+          longitude:74.8560,
+          latitudeDelta:0.01, //basically zoom levels
+          longitudeDelta: 0.01
+      }}/>
     )
 }
 
 export default Map
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    map:{
+        height:300
+    }
+})
