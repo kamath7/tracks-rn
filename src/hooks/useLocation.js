db913abc91e187255decbe11bbcb5db65ca70754
kamath7 <adithyakamath96@gmail.com>
2021-08-26 00:03:34 +0530

    creating new hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,34 @@
+import {
+  Accuracy,
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
+
+//custom hook
+export default (callback) => {
+  const [permError, setPermError] = useState(null);
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000, //update every second
+          distanceInterval: 10,
+        },
+        callback
+      );
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+    } catch (e) {
+      setPermError(e);
+    }
+  };
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return [permError];
+};
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,39 +1,14 @@
-import React, { useEffect, useState, useContext } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Map from "../components/Map";
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from "expo-location";
+import useLocation from "../hooks/useLocation";
 // import "../mockLocations";
-import {Context as LocationContext} from '../context/locationContext'
+import { Context as LocationContext } from "../context/locationContext";
 const TrackCreateScreen = () => {
-  const {addLocation} = useContext(LocationContext)
-  const [permError, setPermError] = useState(null);
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      await watchPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 1000, //update every second
-        distanceInterval: 10,
-      },(location)=>{
-          addLocation(location)
-      });
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-    } catch (e) {
-      setPermError(e);
-    }
-  };
-  useEffect(() => {
-    startWatching();
-  }, []);
-
+  const { addLocation } = useContext(LocationContext);
+  const [permError] = useLocation(addLocation);
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h3>Track Create Screen</Text>
