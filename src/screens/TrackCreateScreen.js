2e8be0b8ee876587136766071138f7b1b5b69502
kamath7 <adithyakamath96@gmail.com>
2021-08-26 00:09:41 +0530

    building cleanup func

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,22 +2,25 @@ import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { withNavigationFocus } from "react-navigation";
 import Map from "../components/Map";
 import useLocation from "../hooks/useLocation";
 // import "../mockLocations";
 import { Context as LocationContext } from "../context/locationContext";
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({isFocused}) => {
   const { addLocation } = useContext(LocationContext);
   const [permError] = useLocation(addLocation);
+  console.log(isFocused)
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h3>Track Create Screen</Text>
       <Map />
+
       {permError ? <Text>Please set permissions</Text> : null}
     </SafeAreaView>
   );
 };
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
 
 const styles = StyleSheet.create({});
