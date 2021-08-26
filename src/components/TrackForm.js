f334091d6937381906437d1cc80d74f3758cc0ab
kamath7 <adithyakamath96@gmail.com>
2021-08-26 20:04:40 +0530

    edits to track form

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import {Button, Input} from 'react-native-elements'
-import Spacer from '../components/Spacer'
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input } from "react-native-elements";
+import Spacer from "../components/Spacer";
+import { Context as LocationContext } from "../context/locationContext";
 const TrackForm = () => {
-    return (
-        <>
-        <Spacer>
-         <Input placeholder="Enter track name"/>
-         </Spacer>
-         <Button title="Start Recording"/>  
-        </>
-    )
-}
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
+  } = useContext(LocationContext);
+  console.log(locations.length)
+  return (
+    <>
+      <Spacer>
+        <Input
+          value={name}
+          placeholder="Enter track name"
+          onChangeText={changeName}
+        />
+      </Spacer>
+      {!recording ? (
+        <Button title="Start Recording" onPress={startRecording} />
+      ) : (
+        <Button title="Stop Recording" onPress={stopRecording} />
+      )}
+    </>
+  );
+};
 
-export default TrackForm
+export default TrackForm;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
