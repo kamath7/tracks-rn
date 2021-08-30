a5f8cea368880497a89eae29af25b55eb3d06960
kamath7 <adithyakamath96@gmail.com>
2021-08-31 01:26:33 +0530

    saving tracks and custom hook

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,4 +1,20 @@
-import axios from 'axios'
-export default axios.create({
-    baseURL: 'https://tracks-backend.herokuapp.com'
-})
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const instance = axios.create({
+  baseURL: "https://tracks-backend.herokuapp.com",
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+); //executed before sending a request to the above URL
+//1st func auto called, 2nd func err with the req
+export default instance;
